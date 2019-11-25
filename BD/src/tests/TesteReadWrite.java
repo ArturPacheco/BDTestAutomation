5e922f96570f426233f4f029e108bbaef9c751aa
Artur Pacheco <arturpacheko@gmail.com>
2019-11-25 21:50:53 -0200

    Teste para JUnit

---
+++ BD/src/tests/TesteReadWrite.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TesteConversor extends TestCase {
+	
+	@Test
+	public void justDoIt() {
+		assertEquals("stringss", "stringss");
+	}
+
+}
