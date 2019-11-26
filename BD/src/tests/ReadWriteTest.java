83617202ed417e0f092842e2e9ece552462acaeb
Artur Pacheco <arturpacheko@gmail.com>
2019-11-25 22:38:46 -0200

    Teste com Junit

---
+++ BD/src/tests/ReadWriteTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ReadWriteTest extends TestCase{
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
